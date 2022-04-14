094e03b89f7ac45aba21f37033cadfa93096a94e
FlutLab.io <>
2022-04-14 15:25:00 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.black,
-      ),S
+      ),
       onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
