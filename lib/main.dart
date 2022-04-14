39295436df98e09a0a16525184175f463b1e620d
FlutLab.io <>
2022-04-14 15:27:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:belajarform/route.dart';
 
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
